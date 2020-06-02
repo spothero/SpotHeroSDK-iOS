f5e038f8ab9f4d03dc0803a7c51182b8c0698331
Brian Drelling <brian.drelling@gmail.com>
2020-06-02 13:23:34 -0500

    Included files from the latest POC revision

---
+++ Sources/SpotHeroAPI/Protocols/RequestDefining.swift
@@ -0,0 +1,16 @@
+// Copyright © 2020 SpotHero, Inc. All rights reserved.
+
+import Foundation
+import UtilityBeltNetworking
+
+protocol RequestDefining {
+    associatedtype ResponseModel: Decodable
+    associatedtype Route
+    
+    static var method: HTTPMethod { get }
+    static var route: Route { get }
+    
+    var client: NetworkClient { get }
+    
+    init(client: NetworkClient)
+}
