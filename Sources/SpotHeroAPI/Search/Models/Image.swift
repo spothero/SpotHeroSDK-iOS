2b08f7ba54d40895e6b96cb5ab2338ed5137998f
Brian Drelling <brian.drelling@gmail.com>
2020-06-17 14:24:14 -0500

    Removed alt text

--- Sources/SpotHeroAPI/Search/Models/Image.swift
+++ Sources/SpotHeroAPI/Search/Models/Image.swift
@@ -2,12 +2,14 @@
 
 /// Representation of a displayable image.
 public struct Image: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case url
+    }
+    
     /// ID which uniquely identifies an image; an opaque string.
     public let id: String
     
     /// URL from which image can be loaded.
     public let url: String
-    
-    /// Caption associated with the image.
-    public let caption: String
 }
