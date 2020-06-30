011294fd6855eea36b89357aa5f96e4efeabce62
Brian Drelling <brian.drelling@gmail.com>
2020-06-30 11:45:49 -0500

    Resolved line length warning

--- Sources/SpotHeroAPI/Search/Models/MonthlyReservationDates.swift
+++ Sources/SpotHeroAPI/Search/Models/MonthlyReservationDates.swift
@@ -4,9 +4,13 @@ import Foundation
 
 /// Represents a pair of start/end dates.
 public struct MonthlyReservationDates: Codable {
-    /// Represents the instant at which a reservation starts. Localized in the time zone of the facility. Formatted as an ISO-8601 (RFC 3339) datetime.
+    /// Represents the instant at which a reservation starts.
+    /// Localized in the time zone of the facility.
+    /// Formatted as an ISO-8601 (RFC 3339) datetime.
     public let starts: Date
     
-    /// Represents the instant at which a reservation ends. Localized in the time zone of the facility. Formatted as an ISO-8601 (RFC 3339) datetime.
+    /// Represents the instant at which a reservation ends.
+    /// Localized in the time zone of the facility.
+    /// Formatted as an ISO-8601 (RFC 3339) datetime.
     public let ends: Date
 }
