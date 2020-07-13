5ce78d23f5e8c00641426b9f39572d1ee6a95f62
Brian Drelling <brian.drelling@gmail.com>
2020-07-13 17:03:53 -0500

    Removed billing from docs

--- Sources/SpotHeroAPI/Search/Enums/MonthlyStartDateRestriction.swift
+++ Sources/SpotHeroAPI/Search/Enums/MonthlyStartDateRestriction.swift
@@ -2,12 +2,12 @@
 
 /// Represents a restriction on the start date of the monthly reservation.
 public enum MonthlyStartDateRestriction: String, Codable {
-    /// Billing can only start on the 1st of the month.
+    /// Reservation can only start on the 1st of the month.
     case firstOnly = "1st_only"
     
-    /// Billing may start on the 1st or 15th of the month.
+    /// Reservation may start on the 1st or 15th of the month.
     case firstOrFifteenth = "1st_or_15th"
     
-    /// There is no restriction on when billing can start.
+    /// There is no restriction on when the reservation can start.
     case none
 }
