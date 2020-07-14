be757a72213991e425892869991c658fc4af9fa1
Brian Drelling <brian.drelling@gmail.com>
2020-07-14 12:36:44 -0500

    Additional WIP

--- Sources/SpotHeroAPI/Search/Models/Distance.swift
+++ Sources/SpotHeroAPI/Search/Models/Distance.swift
@@ -4,15 +4,8 @@
 public struct Distance: Codable {
     private enum CodingKeys: String, CodingKey {
         case linearMeters = "linear_meters"
-        case walkingMeters = "walking_meters"
     }
     
     /// The linear distance in meters between the facility and search origin.
     public let linearMeters: Double?
-    
-    // WIP: This might be removed, validate before the end of the spec.
-    /// The walking distance in meters between the facility and search origin.
-    /// This field is only populated for facilities nearest to the search origin.
-    /// For more distant facilities, it will be set to null.
-    public let walkingMeters: Double?
 }
