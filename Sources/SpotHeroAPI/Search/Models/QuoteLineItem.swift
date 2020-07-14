9e23503a3ee55f0bdf423535e6623f836c6bb78e
Brian Drelling <bdrelling@users.noreply.github.com>
2020-07-14 11:54:29 -0500

    Updated Quote response model (#15)

---
+++ Sources/SpotHeroAPI/Search/Models/QuoteLineItem.swift
@@ -0,0 +1,23 @@
+// Copyright © 2020 SpotHero, Inc. All rights reserved.
+
+/// Represents a line item within an quote.
+public struct QuoteLineItem: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case price
+        case type
+        case shortDescription
+        case fullDescription
+    }
+    
+    // WIP: Missing Docs
+    public let price: Currency
+    
+    /// Origin, purpose, or explanation for the line item.
+    public let type: String
+    
+    /// Abridged human-readable description of the line item.
+    public let shortDescription: String
+    
+    /// Full-length human-readable description of the line item.
+    public let fullDescription: String
+}
