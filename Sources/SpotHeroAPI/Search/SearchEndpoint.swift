3b6606b99f110c58fae2aab9156a04e7e0246af4
Brian Drelling <bdrelling@users.noreply.github.com>
2020-08-24 12:29:22 -0500

    IOS-2253: Added SearchEndpoint (#36)

---
+++ Sources/SpotHeroAPI/Search/SearchEndpoint.swift
@@ -0,0 +1,23 @@
+// Copyright © 2020 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+public final class SearchEndpoint: Endpoint {
+    public let getAirportFacility: SearchGetAirportFacilityRequest
+    public let getAirportFacilities: SearchGetAirportFacilitiesRequest
+    public let getMonthlyFacility: SearchGetMonthlyFacilityRequest
+    public let getMonthlyFacilities: SearchGetMonthlyFacilitiesRequest
+    public let getTransientFacility: SearchGetTransientFacilityRequest
+    public let getTransientFacilities: SearchGetTransientFacilitiesRequest
+    
+    override init(client: NetworkClient) {
+        self.getAirportFacility = SearchGetAirportFacilityRequest(client: client)
+        self.getAirportFacilities = SearchGetAirportFacilitiesRequest(client: client)
+        self.getMonthlyFacility = SearchGetMonthlyFacilityRequest(client: client)
+        self.getMonthlyFacilities = SearchGetMonthlyFacilitiesRequest(client: client)
+        self.getTransientFacility = SearchGetTransientFacilityRequest(client: client)
+        self.getTransientFacilities = SearchGetTransientFacilitiesRequest(client: client)
+        
+        super.init(client: client)
+    }
+}
