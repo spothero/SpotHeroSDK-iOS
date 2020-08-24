f0f5df5cd52963d8d39fa93961b9053f2122fc25
Brian Drelling <brian.drelling@gmail.com>
2020-08-23 22:48:13 -0500

    Added Environment-based configuration to SpotHeroAPIClient

--- Sources/SpotHeroAPI/Core/Helpers/SpotHeroAPIClient.swift
+++ Sources/SpotHeroAPI/Core/Helpers/SpotHeroAPIClient.swift
@@ -5,16 +5,46 @@ import UtilityBeltNetworking
 
 /// A network client for communicating with the SpotHero API.
 public final class SpotHeroAPIClient {
+    // MARK: Enums
+    
+    /// The API environment for all network requests.
+    public enum Environment {
+        /// Represents the SpotHero mobile staging environment.
+        case mobileStaging
+        /// Represents the SpotHero staging environment.
+        case staging
+        /// Represents the SpotHero production environment.
+        case production
+        /// Represents a SpotHero sandbox environment, which requires a URL.
+        case sandbox(url: URLConvertible)
+        
+        /// The base URL for the API.
+        var baseURL: URLConvertible {
+            switch self {
+            case .mobileStaging:
+                return "https://api.sandbox.spothero.com"
+            case .staging:
+                return ""
+            case .production:
+                return ""
+            case let .sandbox(url):
+                return url
+            }
+        }
+    }
+    
     // MARK: Properties
     
-    let networkClient: NetworkClient
-    
-    // WIP: Endpoint definitions will be added here
+    /// Represents the `/search` endpoint.
+    public let search: SearchEndpoint
     
     // MARK: Methods
     
     /// Creates a new instance of `SpotHeroAPIClient`.
-    public init(baseURL: String) {
-        self.networkClient = NetworkClient(baseURL: baseURL)
+    public init(environment: Environment) {
+        let networkClient = NetworkClient(baseURL: environment.baseURL)
+        
+        /// V2 Endpoints
+        self.search = SearchEndpoint(client: networkClient)
     }
 }
