3578afb109e242283dddd9cb5048843c0532f486
Brian Drelling <brian.drelling@gmail.com>
2020-11-16 14:09:35 -0600

    Added early bird information

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes+EarlyBird.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes+EarlyBird.swift
@@ -1,11 +1,30 @@
 // Copyright © 2020 SpotHero, Inc. All rights reserved.
 
+import Foundation
+
 public extension TransientFacilityAttributes {
     /// Contains all fields relevant to a facility’s cancellation policy.
     struct EarlyBird: Codable {
         private enum CodingKeys: String, CodingKey {
             case description
-            case enterPriod = "enter_period"
+            case enterPeriod = "enter_period"
         }
+        
+        /// The human readable description of the early bird rate.
+        public let description: String
+        
+        /// Store the enter start/end times for the early bird rate.
+        public let enterPeriod: EnterPeriod
+    }
+}
+
+public extension TransientFacilityAttributes.EarlyBird {
+    /// Store the enter start/end times for the early bird rate.
+    struct EnterPeriod: Codable {
+        /// The enter start time for the early bird rate in the format XX:XX AM/PM with leading 0’s removed.
+        public let starts: Date
+        
+        /// The enter end time for the early bird rate in the format XX:XX AM/PM with leading 0’s removed.
+        public let ends: Date
     }
 }
