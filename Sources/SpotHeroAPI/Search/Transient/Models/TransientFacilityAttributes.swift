ac0d060a99df2b8e502ecd58c46368c569b4a634
Brian Drelling <bdrelling@users.noreply.github.com>
2020-11-16 14:39:21 -0600

    IOS-2614: Added Early Bird information to the Transient facility (#49)

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
@@ -4,12 +4,17 @@
 public struct TransientFacilityAttributes: Codable {
     private enum CodingKeys: String, CodingKey {
         case cancellation
+        case earlyBird = "early_bird"
         case isCommutedCardEligible = "is_commuter_card_eligible"
         case redemptionInstructions = "redemption_instructions"
     }
     
     /// Contains all fields relevant to a facility’s cancellation policy.
     public let cancellation: Cancellation
+    
+    /// Stores rate data specific to the early bird rate, if applicable.
+    /// If the returned rate does not represent an early bird rate, this object will be `nil`.
+    public let earlyBird: EarlyBird?
     
     /// Indicates whether a commuter benefits cards is eligible to be used at this facility for the work location supplied on the request.
     public let isCommutedCardEligible: Bool
