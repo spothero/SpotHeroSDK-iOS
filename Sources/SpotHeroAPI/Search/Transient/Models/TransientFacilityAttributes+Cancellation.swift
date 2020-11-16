461c10cec23381a3c237c9e78d1cfbadfb780bd3
Brian Drelling <brian.drelling@gmail.com>
2020-11-16 12:52:21 -0600

    Fixed swiftlint violation

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes+Cancellation.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes+Cancellation.swift
@@ -12,7 +12,8 @@ public extension TransientFacilityAttributes {
         /// Whether or not the reservation can be cancelled by the customer.
         public let allowedByCustomer: Bool
         
-        /// Whether or not the reservation can be cancelled by SpotHero’s customer service team. Will always be `true` when `allowedByCustomer` is `true`.
+        /// Whether or not the reservation can be cancelled by SpotHero’s customer service team.
+        /// Will always be `true` when `allowedByCustomer` is `true`.
         public let allowedBySpotHeroCustomerService: Bool
         
         /// The number of minutes before a reservation starts at which point cancellation is no longer allowed.
