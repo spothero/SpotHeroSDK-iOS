0821d99f8e6de41cdbec08670d471fbab342beea
Brian Drelling <brian.drelling@gmail.com>
2020-11-18 14:50:35 -0600

    Moving cancellation to match change in spec

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
@@ -3,13 +3,9 @@
 /// Represents facility information only applicable within the transient context.
 public struct TransientFacilityAttributes: Codable {
     private enum CodingKeys: String, CodingKey {
-        case cancellation
         case isCommutedCardEligible = "is_commuter_card_eligible"
         case redemptionInstructions = "redemption_instructions"
     }
-    
-    /// Contains all fields relevant to a facility’s cancellation policy.
-    public let cancellation: Cancellation
     
     /// Indicates whether a commuter benefits cards is eligible to be used at this facility for the work location supplied on the request.
     public let isCommutedCardEligible: Bool
