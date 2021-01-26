4290bfee52f498c41b2afdcec7ee1733252e59ca
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2021-01-26 15:44:20 -0600

    Start using the module bundle

--- Tests/SpotHeroAPITests/Core/StubResponse+APIMockFile.swift
+++ Tests/SpotHeroAPITests/Core/StubResponse+APIMockFile.swift
@@ -1,19 +1,22 @@
 // Copyright © 2021 SpotHero, Inc. All rights reserved.
 
+import Foundation
 import Sham
 import UtilityBeltNetworking
 
 extension StubResponse {
     /// Returns a SpotHeroAPI mock file. This abstraction layer allows easy conversion if the directory changes,
     /// or when Swift Package Manager can successfully work with resource bundles.
-    /// - Parameter file: The name of the mock file.
+    /// - Parameter file: The name of the mock file, without the .json extension.
     /// - Parameter statusCode: The HTTP status code to return. Defaults to 200 OK.
     /// - Parameter headers: The headers to return in the response.
     static func apiMockFile(_ file: String,
                             statusCode: HTTPStatusCode = .ok,
                             headers: [String: String] = [:]) -> StubResponse {
-        return .relativeFile("../Resources/MockFiles/\(file)",
-                             statusCode: statusCode,
-                             headers: headers)
+        return .resource(file,
+                         fileExtension: "json",
+                         bundle: .module,
+                         statusCode: statusCode,
+                         headers: headers)
     }
 }
