7b993e446dfedb506ca7b4ae265547831fef32ed
Brian Drelling <bdrelling@users.noreply.github.com>
2021-02-25 14:24:50 -0600

    Updated the Transient spec for commuted card eligibility (#68)

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientFacilityAttributes.swift
@@ -3,12 +3,8 @@
 /// Represents facility information only applicable within the transient context.
 public struct TransientFacilityAttributes: Codable {
     private enum CodingKeys: String, CodingKey {
-        case isCommutedCardEligible = "is_commuter_card_eligible"
         case redemptionInstructions = "redemption_instructions"
     }
-    
-    /// Indicates whether a commuter benefits cards is eligible to be used at this facility for the work location supplied on the request.
-    public let isCommutedCardEligible: Bool
     
     /// Information concerning the redemption process for customers who park at a facility.
     public let redemptionInstructions: TransientRedemptionInstructions
