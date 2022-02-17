94c239d8b0711cb0eb371207c2dc5b6dffeffb01
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2022-02-17 15:46:22 -0600

    Ran swiftformat

--- Sources/SpotHeroAPI/Search/Common/Models/CommonFacilityAttributes.swift
+++ Sources/SpotHeroAPI/Search/Common/Models/CommonFacilityAttributes.swift
@@ -1,4 +1,4 @@
-// Copyright © 2021 SpotHero, Inc. All rights reserved.
+// Copyright © 2022 SpotHero, Inc. All rights reserved.
 
 /// Represents common facility information applicable in all contexts.
 public struct CommonFacilityAttributes: Codable {
