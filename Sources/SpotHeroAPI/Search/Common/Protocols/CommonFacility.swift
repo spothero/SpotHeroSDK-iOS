6afe29239d2381e50ee15aa1a45ea83dedc8eb5a
Luke Quigley <luke@spothero.com>
2022-05-03 13:42:59 -0500

    Creates a common facility protocol

---
+++ Sources/SpotHeroAPI/Search/Common/Protocols/CommonFacility.swift
@@ -0,0 +1,7 @@
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
+
+/// Representation of a facility.
+public protocol CommonFacility: Codable {
+    /// Represents common facility information applicable in all contexts.
+    var common: CommonFacilityAttributes { get }
+}
