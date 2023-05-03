7fc7ac831c84d968365fc1d8d97be9f79a423f60
Kevin Delannoy <kevin.delannoy@spothero.com>
2023-05-03 09:18:10 -0400

    IOS-4087: Added available spaces

--- Sources/SpotHeroAPI/Search/Common/Models/Availability.swift
+++ Sources/SpotHeroAPI/Search/Common/Models/Availability.swift
@@ -1,9 +1,17 @@
-// Copyright © 2022 SpotHero, Inc. All rights reserved.
+// Copyright © 2023 SpotHero, Inc. All rights reserved.
 
 import Foundation
 
 /// A model representing availability of a given parking facility.
 public struct Availability: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case available
+        case availableSpaces = "available_spaces"
+    }
+
     /// The boolean that informs us whether the spot is available
     public let available: Bool
+
+    /// The number of available spots in the facility or nil
+    public let availableSpaces: Int?
 }
