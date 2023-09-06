9877563a8787d926c4f5930b96c1132f5779de88
Michael Polyakov <141768546+mikepoly@users.noreply.github.com>
2023-09-06 12:31:17 -0400

    IOS-4354: added the group title to the transient search parsing. (#106)
    * IOS-4354: added the group title to the transient search parsing.
    
    * IOS-4354: fix lint

--- Sources/SpotHeroAPI/Search/Transient/Models/TransientRate.swift
+++ Sources/SpotHeroAPI/Search/Transient/Models/TransientRate.swift
@@ -1,4 +1,4 @@
-// Copyright © 2022 SpotHero, Inc. All rights reserved.
+// Copyright © 2023 SpotHero, Inc. All rights reserved.
 
 /// Transient-specific metadata pertaining to a rate for the rental of a parking spot.
 public struct TransientRate: Codable {
@@ -6,6 +6,7 @@ public struct TransientRate: Codable {
         case amenities
         case earlyBird = "early_bird"
         case redemptionType = "redemption_type"
+        case ruleGroupTitle = "rule_group_title"
     }
     
     /// Transient parking amenities offered at the facility.
@@ -19,4 +20,7 @@ public struct TransientRate: Codable {
     /// See Redemption Types in the README for a list of supported types.
     /// https://github.com/spothero/SpotHeroSDK-iOS/#redemption-types
     public let redemptionType: String
+    
+    /// The title of the rule group.
+    public let ruleGroupTitle: String
 }
